46faf4cfba1a01fd7c089b08aebbb475a8691c06
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-11-02 20:24:14 -0700

    initialize Firebase

---
+++ assets/javascript/scheduler.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCrqH5EhH6_H91hCtnUT8fSgTCzCm2szwo",
+    authDomain: "train-scheduler-4f471.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4f471.firebaseio.com",
+    projectId: "train-scheduler-4f471",
+    storageBucket: "train-scheduler-4f471.appspot.com",
+    messagingSenderId: "925635835251",
+    appId: "1:925635835251:web:ef7f58d7aa2522355a4106",
+    measurementId: "G-QR91SE5WT8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
